91f4b17150906c4c2810f3d3ca77657d58b7a3a9
Peter Drinnan <petedrinnan@gmail.com>
2025-11-27 23:04:09 -0500

    chore: Update CACHE_NAME to version 1.1.3 in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for WordWalker PWA
-const CACHE_NAME = 'wordwalker-v1.1.2';
+const CACHE_NAME = 'wordwalker-v1.1.3';
 const ASSETS_CACHE = 'wordwalker-assets-v1';
 const AUDIO_CACHE = 'wordwalker-audio-v1';
 const IMAGE_CACHE = 'wordwalker-images-v1';
