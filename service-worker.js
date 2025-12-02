06b8f7d6946436e99a9cec2492073ccbcd24de25
Peter Drinnan <petedrinnan@gmail.com>
2025-12-02 00:15:10 -0500

    feat: Bump version to 1.3.3 and update service worker cache name

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for WordWalker PWA
-const CACHE_NAME = 'wordwalker-v1.3.1';
+const CACHE_NAME = 'wordwalker-v1.3.2';
 const ASSETS_CACHE = 'wordwalker-assets-v1';
 const AUDIO_CACHE = 'wordwalker-audio-v1';
 const IMAGE_CACHE = 'wordwalker-images-v1';
