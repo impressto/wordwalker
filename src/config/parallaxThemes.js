ad7231e1c0042de21d9a75619d7f9268fa73b094
Peter Drinnan <petedrinnan@gmail.com>
2025-12-03 00:01:04 -0500

    feat: Update version to 1.3.7 and add Dia de los Muertos theme assets to service worker

--- src/config/parallaxThemes.js
+++ src/config/parallaxThemes.js
@@ -72,8 +72,8 @@ const parallaxThemes = {
       layer3: -20,    // Buildings/structures - adjust as needed
       layer4: -140,   // Mid-distant buildings - adjust as needed
       layer5: -90,    // high rises in the distance - adjust as needed
-      layer6: 120,   // mountains- adjust as needed
-      layer7: -100,      // Sky/rear - adjust as needed
+      layer6: 80,   // mountains- adjust as needed
+      layer7: 0,      // Sky/rear - adjust as needed
     },
     layerSpeeds: {
       layer1: 0.8,    // Foreground
